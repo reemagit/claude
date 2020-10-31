ad65949194bb3a51bff7c75850b2aaa19d56a9b9
Enrico Maiorino <enrico.maiorino@gmail.com>
2020-10-31 14:33:39 -0400

    Package metadata

---
+++ claude/__init__.py
@@ -0,0 +1,3 @@
+from .base import *
+from . import constraints
+from . import observables
--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='Claude',
-    url='https://github.com/jladan/package_demo',
-    author='John Ladan',
-    author_email='jladan@uwaterloo.ca',
+    name='claude',
+    url='https://github.com/reemagit/claude',
+    author='Enrico Maiorino',
+    author_email='enrico.maiorino@gmail.com',
     # Needed to actually package something
-    packages=['measure'],
+    packages=['claude'],
     # Needed for dependencies
-    install_requires=['numpy'],
+    install_requires=['numpy', 'scipy', 'tqdm'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
     license='MIT',
-    description='An example of a python package from pre-existing code',
+    description='Exponential Random Graph models as null models for network analysis',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
